4598ef0fbacf2b861dd94d9154c7e44785a8d8a7
Rugved789 <rugved.gadge.789@gmail.com>
2026-01-01 23:19:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route ,Routes } from "react-router-dom";
+import NotFound from "./Pages/Notfound";
+import Home from "./Pages/Home";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,24 @@
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggler*/}
+
+
+
+      {/* Background effects*/}
+
+
+
+      {/* Navbar*/}
+
+
+
+      {/* Main*/}
+
+
+      {/* Foteer */}
+    </div>
+  );
+};
+
+export default Home;
